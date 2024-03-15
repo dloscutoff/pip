987bf47a239b31f4207073d761eb346227c961cd
David Loscutoff <dloscutoff@gmail.com>
2024-03-15 17:07:00 -0500

    Fixed #68.

--- execution.py
+++ execution.py
@@ -2801,6 +2801,13 @@ class ProgramState:
                 result = lhs
                 for item in rhs:
                     result = self.REMOVE(result, item)
+            elif rhs is nil:
+                # Nothing to remove, so return the original value
+                result = lhs
+            else:
+                self.err.warn("Unimplemented argtypes for REMOVE:",
+                              type(lhs), "and", type(rhs))
+                return nil
             return Scalar(result)
         elif isinstance(lhs, PipIterable):
             result = list(lhs)
@@ -2812,8 +2819,7 @@ class ProgramState:
         else:
             self.err.warn("Unimplemented argtypes for REMOVE:",
                           type(lhs), "and", type(rhs))
-            # Nothing to remove, so return the original value
-            return lhs
+            return nil
 
     def REPR(self, rhs):
         if isinstance(rhs, Block):
