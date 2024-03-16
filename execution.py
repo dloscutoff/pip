b565e8c7f79d8458652e87a68a4c70ad7d0ab120
David Loscutoff <dloscutoff@gmail.com>
2024-03-15 21:29:56 -0500

    Fixed some operators that returned None instead of nil.

--- execution.py
+++ execution.py
@@ -1187,6 +1187,7 @@ class ProgramState:
         else:
             self.err.warn("Unimplemented argtype for DOUBLE:",
                           type(rhs))
+            return nil
 
     def ENUMERATE(self, iterable):
         if isinstance(iterable, PipIterable):
@@ -1528,6 +1529,7 @@ class ProgramState:
         else:
             self.err.warn("Unimplemented argtype for HALVE:",
                           type(rhs))
+            return nil
 
     def IDENTITYMATRIX(self, rhs):
         if isinstance(rhs, Scalar):
@@ -1540,6 +1542,7 @@ class ProgramState:
         else:
             self.err.warn("Unimplemented argtype for IDENTITYMATRIX:",
                           type(rhs))
+            return nil
 
     def IFTE(self, test, trueBranch, falseBranch):
         # Ternary if-then-else operator
